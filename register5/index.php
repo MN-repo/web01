MN-repo/web01

commit 7dea1d3ef8cd3965350993ac5bb66cbae9767ef3

calls can now be forwarded to SIP addresses, iNums
We now leverage Catapult's support for SIP by allowing users to
provide a SIP address for call forwarding.  We also convert iNums into
their corresponding SIP address, which gives us iNum forwarding too.

The main logic for this is in register5/index.php, with a minor update
to register7/index.php for getting the number into "tel:<E.164_num>"
form.  Our internal format for phone number and SIP address storage
(in catapult_fwd-* values) is similar to Catapult's, except that we
always use a URI, while Catapult chooses to use just the <E.164_num>
part for storing E.164 phone numbers.

The rest of the changes are wording fixes so the user knows SIP and
iNum forwarding is available, which includes an update to the pricing
section that mentions the increased number of forwarding minutes
available to those using SIP or iNum for their call forwarding.

Adding SIP forwarding also necessitated a jmp-fwdcalls change, which
has already been added - see jmp-fwdcalls commit 5161529.

register5/index.php
@@ -64,15 +64,33 @@ Session ID and/or number empty.  Please <a href="../">start again</a>.
 	is_numeric(substr($_GET['number'], 1))) {
 
 	# first, normalize the fwdphone
-	$clean_fwdphone = preg_replace('/[^0-9]/', '', $_GET['fwdphone']);
 
-	if (strlen($clean_fwdphone) == 11 && $clean_fwdphone[0] == '1') {
-		$clean_fwdphone = substr($clean_fwdphone, 1);
+	$clean_fwdphone = '';
+
+	# see if it's a likely SIP address
+	if (strpos($_GET['fwdphone'], '@') !== FALSE) {
+		$clean_fwdphone = 'sip:'.$_GET['fwdphone'];
+	} else {
+		$clean_fwdphone = preg_replace('/[^0-9]/','',$_GET['fwdphone']);
+
+		if (strlen($clean_fwdphone) == 11 && $clean_fwdphone[0] == '1'){
+			$clean_fwdphone = substr($clean_fwdphone, 1);
+		}
+
+		if (strlen($clean_fwdphone) == 15 &&
+			substr($clean_fwdphone, 0, 7) == '8835100') {
+
+			$clean_fwdphone =
+				'sip:'.$clean_fwdphone.'@sip.inum.net';
+		} else if (strlen($clean_fwdphone) != 10) {
+			# for E.164 numbers, we only currently support iNum/NANP
+			$clean_fwdphone = '';
+		} else {
+			$clean_fwdphone = '+1'.$clean_fwdphone;
+		}
 	}
 
-	if (strlen($clean_fwdphone) == 10) {
-		$clean_fwdphone = '+1'.$clean_fwdphone;
-
+	if (!empty($clean_fwdphone)) {
 		include '../../../../settings-jmp.php';
 
 		$redis = new Redis();
@@ -185,7 +203,8 @@ If you have not yet received the verification code, please <a href=
 ?>
 ) is not a <a href="https://en.wikipedia.org/wiki/North_American_Numbering_Plan"
 >NANP</a> phone number (ie. +1 800 622 6232 or (800) 622-6232 - any format is
-acceptable).  Please press Back and try a different number or
+acceptable), a SIP URI (ie. sip_user@example.com), or an iNum (ie. +883 510 000
+000 094).  Please press Back and try a different number or
 <a href="../">start again</a>.
 <?php
 	}

register4/index.php
@@ -111,24 +111,25 @@ Your JID (<?php echo htmlentities($jid) ?>) has been successfully verified.
 </p>
 
 <p>
-Please enter a phone number that will receive all voice calls delivered to your
-JMP number.  This phone number will receive a call when you press Submit and you
+Please enter a phone number (including <a href="https://www.inum.net/">iNum</a>)
+or SIP address that will receive all voice calls delivered to your JMP number.
+This phone number or SIP address will receive a call when you press Submit; you
 will enter the verification code that it provides to you on the next page.
 </p>
 
 <p>
-<b>Note:</b> You may choose not to enter a phone number - in that case any calls
+<b>Note:</b> You may choose to leave the below box empty; in that case any calls
 to your JMP number will receive a pre-recorded message saying "This phone number
 does not receive voice calls; please send a text message instead" and the caller
 will not be able to leave a voicemail. Please just click Submit without entering
-a phone number if that is your preference.
+anything in the box if that is your preference.
 </p>
 
 <form action="../register5/">
 <p>
 <input type="hidden" name="number" value="<?php echo $_GET['number'] ?>" />
 <input type="hidden" name="sid" value="<?php echo $clean_sid ?>" />
-Phone number that receives calls: <input type="text" name="fwdphone" />
+Number or SIP address that receives calls: <input type="text" name="fwdphone" />
 <input type="submit" value="Submit" />
 </p>
 </form>

register7/index.php
@@ -68,6 +68,11 @@ href="xmpp:discuss@conference.soprani.ca?join">discuss@conference.soprani.ca</a>
 	$phone = $redis->get('reg-phn_good-'.$_GET['jmp-sid']);
 	$jid = $redis->get('reg-jid_good-'.$_GET['jmp-sid']);
 
+	# Catapult naming is used until this point - here we switch to URI form
+	if (!is_null($phone) && !empty($phone) && $phone[0] == '+') {
+		$phone = 'tel:'.$phone;
+	}
+
 
 	# log the GET params - put in a variable keyed on transaction ID
 	$pLog = '';
@@ -366,10 +371,15 @@ received/sent from your Jabber ID (<?php echo $jid ?>) while calls to
 "This phone number does not receive voice calls; please send a text message
 instead".
 <?php
-				} else {
+				} elseif (substr($phone, 0, 4) == 'sip:') {
 ?>
-<?php echo $_GET['jmp-number'] ?> will be forwarded to <?php echo $phone ?>.
-<?php
+<?php echo $_GET['jmp-number'] ?> will be forwarded to this SIP address: <?php
+					echo substr($phone, 4)." .\n";
+
+				} else {  # substr($phone, 0, 4) == 'tel:'
+?>
+<?php echo $_GET['jmp-number'] ?> will be forwarded to <?php
+					echo substr($phone, 4).".\n";
 				}
 ?>
 </p><p>

index.html
@@ -72,10 +72,12 @@ free and open servers available which you can use with any client; one of the sm
 </p>
 
 <p>
-<em>If</em> you want to enable incoming voice calls, you will need an existing
-Canadian or US phone number (can be voice-only, no Touch-Tone needed) for call forwarding;
-forwarding to <a href="https://arstechnica.com/business/2010/01/voip-in-depth-an-introduction-to-the-sip-protocol-part-1/">SIP</a>
-and voicemail right in your chat history are <a href="notify_signup/">in the works</a>.
+<em>If</em> you want to enable incoming voice calls, you will need one of the
+following for call forwarding: an existing Canadian or US phone number (can be
+voice-only, no Touch-Tone needed), an <a href="https://www.inum.net/">iNum</a>,
+or a <a href="https://arstechnica.com/business/2010/01/voip-in-depth-an-introduction-to-the-sip-protocol-part-1/">SIP</a>
+address.  Voicemail right in your chat history is
+<a href="notify_signup/">in the works</a>.
 </p>
 
 <p>
@@ -167,7 +169,9 @@ required for now in order to maintain proper uniqueness going forward.
 <p>
 JMP is currently in beta and the introductory rate for beta users is US$2.99 per
 month.  This includes unlimited SMS and MMS, as well as 50 minutes of incoming
-voice calls.
+voice calls (100 minutes if using an <a href="https://www.inum.net/">iNum</a> or
+<a href="https://arstechnica.com/business/2010/01/voip-in-depth-an-introduction-to-the-sip-protocol-part-1/">SIP</a>
+address for call forwarding).
 </p>
 <p>
 Once the beta period for JMP is over, the US$2.99 subscription will still be

commit df8a6fa2b6b96b995a6533b09ba710a5fa79d449

fix "try again" so sip: not double-added; fixes #4
This bug was introduced in 7dea1d3 along with the rest of SIP support.
The issue is that we've always used the normalized number in the "try
again" links, which was fine when we just had E.164 numbers, but now
normalization also includes adding "sip:" in some cases.  But we can't
know whether a string starting with "sip:" has been normalized already
because a SIP address could start with "sip:" (giving a "sip:sip:..."
URI) per https://tools.ietf.org/html/rfc3261#section-19.1.1 .  So we
have to be careful to only use the string the user already gave us
(pre-normalization) in our "try again" links.

register5/index.php
@@ -162,7 +162,7 @@ There was an error calling your number to deliver the code.  Please <a href=
 ?>&amp;sid=<?php
 	echo $clean_sid;
 ?>&amp;fwdphone=<?php
-	echo urlencode($clean_fwdphone);
+	echo urlencode($_GET['fwdphone']);
 ?>">click here</a> to try again or press Back to change your forwarding number.
 <?php
 		        } else {
@@ -191,7 +191,7 @@ If you have not yet received the verification code, please <a href=
 ?>&amp;sid=<?php
 	echo $clean_sid;
 ?>&amp;fwdphone=<?php
-	echo urlencode($clean_fwdphone);
+	echo urlencode($_GET['fwdphone']);
 ?>">click here</a> to try again or press Back to change your forwarding number.
 <?php
 			}