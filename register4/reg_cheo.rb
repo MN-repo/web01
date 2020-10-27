MN-repo/web01

commit 96b68539913346b9c548556b9bc9032ebcb6f27e

major 2nd step of painful signup decoupling - #18
The first major step was in 4a26799, which laid the groundwork for
this.  Thanks to a448170 we didn't need to include any renames here so
it's a fairly explanatory commit.

At a high level, we are trying to decouple the various bits of the
signup process, most of which have been housed in register4 until now.
We are doing this so that it is easier for a user to enter the process
at any given step, which makes it easier to add things like payment
options and the "trial accounts need wait/referral" feature mentioned
in #18.

With this commit we are finally at the point where we at least have
the steps for the base case in their final location.  In particular,
the options1 step is intended to complete the JID verification (which
it does now) and then give users options for how they want to pay/etc.
for their JMP account (which it will do later).  The brand new
wait_for_approval1 step will give users the option to wait for an
admin to approve their JMP trial signup (which is the closest
approximation to the current behaviour, where it's auto-approved), but
for now just continues the auto-approving that was done already.
However, we can much more easily add the "wait" feature, since the
user can just keep reloading this page now, as it only does one thing.

The major changes to make this all happen are:

1. Moving a bunch of stuff from register4 into options1.  Of special
note is the JID verification, which now happens in options1 so we know
at that point whether we can "guarantee" that the person with a given
sid (session ID) owns a given JID.  This makes it easier to reason
about the next steps.

2. Moving some stuff from options1 into wait_for_approval1 (the reason
we needed to create it to begin with).  This is really just the signup
notification code, which lets a custom JID know about new signups that
may need approval.  For now it's notify-only, but eventually will

register4/reg_cheo.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 #
-# Copyright (C) 2017-2018  Denver Gingerich <denver@ossguy.com>
+# Copyright (C) 2017-2018, 2020  Denver Gingerich <denver@ossguy.com>
 #
 # This file is part of jmp-register.
 #
@@ -43,14 +43,10 @@ $q_done = Queue.new
 
 class SApp < Sinatra::Application
 	get '/' do
-		if not params.key?('jcode')
-			$stderr.puts 'vError - no verification code'
-			@error_text = 'Verification code not entered.  Please '\
-				'press Back and enter a verification code or '\
-				'<a href="../">start again</a>.'
-			return erb :error
+		# TODO NOW: MUST confirm that reg attempt for DNE number fails
+		#  spec'ly must fail gracefully - user can retry, no partial reg
 
-		elsif not params.key?('number') or not params.key?('sid')
+		if not params.key?('number') or not params.key?('sid')
 			$stderr.puts 'sError - no sid or number'
 			@error_text = 'Session ID and/or number empty.  Please'\
 				' <a href="../">start again</a>.'
@@ -76,14 +72,15 @@ class SApp < Sinatra::Application
 
 		# TODO: support Redis auth
 
-		jidMaybeKey = 'reg-jid_maybe-' + params['sid']
+		jidDefinitelyKey = 'reg-jid_definitely-' + params['sid']
 
-		conn.write ["GET", jidMaybeKey]
+		conn.write ["GET", jidDefinitelyKey]
 		jid = conn.read
 
 		if jid.nil?
 			$stderr.puts 'jError when trying to verify sid ' +
 				params['sid']
+			# TODO: confirm this text is correct
 			@error_text = 'Could not find JID to verify; perhaps '\
 				'it has already been verified.  Feel free to '\
 				'<a href="../">start again</a> if not.'
@@ -91,53 +88,8 @@ class SApp < Sinatra::Application
 			return erb :error
 		end
 
-		hitsKey = 'reg-jid_hits-' + jid
-		conn.write ["INCR", hitsKey]
-		hitCount = conn.read
 
-		jcodeKey = 'reg-jcode-' + jid
-
-		@cleanSid = params['sid'].gsub(/[^0-9a-f]/, "")
-
-
-		# if > 10 hits, do NOT allow verification to occur (rate limit)
-		if hitCount > 10
-			conn.write ["TTL", hitsKey]
-			if conn.read < 0
-				conn.write ["EXPIRE", hitsKey, 600]
-				conn.read  # TODO: check value to confirm worked
-			end
-
-			$stderr.puts 'oError when trying to verify jid ' +
-				CGI.escapeHTML(jid)
-			@error_text = 'Too many verification attempts.  Please'\
-				' refresh this page in about 10 minutes or '\
-				'<a href="../">start again</a>.'
-			conn.disconnect
-			return erb :error
-		end
-
-		conn.write ["GET", jcodeKey]
-		if params['jcode'].downcase != conn.read
-			$stderr.puts 'iError when trying to verify jid ' +
-				CGI.escapeHTML(jid) + ' with ' +
-				CGI.escapeHTML(params['jcode'])
-			@error_text = '</p>
-<form action="../register4/">
-<p>
-<input type="hidden" name="number" value="' + params['number'] + '" />
-<input type="hidden" name="sid" value="' + @cleanSid + '" />
-Invalid verification code (' + CGI.escapeHTML(params['jcode']) + ').  Please
-enter a new code to try again: <input type="text" name="jcode" />
-<input type="submit" value="Submit" />
-</p>
-</form>
-<p>'
-			conn.disconnect
-			return erb :error
-		end
-
-
+		# TODO NOW: delete below IP check (should be fine with new flow)
 		# confirm that there haven't been too many requests from this IP
 		ipHitsKey = 'reg-ipa_hits-' + request.ip
 		conn.write ["INCR", ipHitsKey]
@@ -178,15 +130,39 @@ enter a new code to try again: <input type="text" name="jcode" />
 			gsub('>', "\\\\3e").
 			gsub('@', "\\\\40") + '@' + $cheogram_jid
 
+
+		paymentPlanKey = 'payment-plan_as_of_' +
+			Date.today.strftime('%Y%m') + '-' + cheo_jid
+
+		conn.write ["GET", paymentPlanKey]
+		payPlan = conn.read
+
+		# TODO: don't hard-code expected payment plan name
+		if payPlan.nil? or payPlan != 'xxx_stable_trial-v20200913'
+			$stderr.puts 'yError when trying to verify plan for ' +
+				jid + ' with plan name "' + payPlan.to_s + '"'
+			# TODO: confirm this text is correct
+			@error_text = 'Trial account has not been approved or '\
+				'other issue with payment plan for this JID.  '\
+				'<a href="../#support">Contact support</a> to '\
+				'fix or <a href="../">start again</a>.'
+			conn.disconnect
+			return erb :error
+		end
+
+
 		credKey = 'catapult_cred-' + cheo_jid
 
+		# TODO: figure out better error message here; think of each case
 		conn.write ["EXISTS", credKey]
 		if conn.read == 1
-			# very unlikely due to check earlier in registration
-			$stderr.puts "cError"
-			# TODO: add "contact support"
-			@error_text = 'This JID is already registered.  Please'\
-				' contact support to use it with a new number.'
+			$stderr.puts 'cError when trying to register JID "' +
+				CGI.escapeHTML(jid) + '" with number ' +
+				params['number'] + ' and sid ' + params['sid']
+			@error_text = 'This JID (' + CGI.escapeHTML(jid) +
+				') is already registered.  Please '\
+				'<a href="../#support">contact support</a> to '\
+				'use it with a new number.'
 			conn.disconnect
 			return erb :error
 		end

options1/index.php
@@ -27,15 +27,9 @@
 <title>JMP</title>
 <?php
 
-if (empty($_GET['jcode'])) {
-?>
-</head>
-<body>
-<p>
-Verification code not entered.  Please press Back and enter a verification code
-or <a href="../">start again</a>.
-<?php
-} elseif (empty($_GET['number'])) {
+# TODO: remove this after options1 handles more cases (some don't need number)
+if (empty($_GET['number'])) {
+	error_log('sError - no JMP number');
 ?>
 </head>
 <body>
@@ -43,6 +37,7 @@ or <a href="../">start again</a>.
 Number not entered.  Please <a href="../">start again</a>.
 <?php
 } elseif (empty($_GET['sid'])) {
+	error_log('sError - no session ID (sid) provided');
 ?>
 </head>
 <body>
@@ -59,76 +54,173 @@ No session ID found.  Please <a href="../">start again</a>.
 		$redis->auth($redis_auth);
 	}
 
-	$jidMaybeKey = 'reg-jid_maybe-'.$_GET['sid'];
-	$jid = $redis->get($jidMaybeKey);
-
-	# TODO NOW: check for empty/null $jid
+	$jidDefinitelyKey = 'reg-jid_definitely-'.$_GET['sid'];
+	$jid = $redis->get($jidDefinitelyKey);
 
 	$clean_sid = preg_replace('/[^0-9a-f]/', '', $_GET['sid']);
-	$jcode = preg_replace('/[^0-9a-f]/', '', $_GET['jcode']);
 
-	# send the registration request message: informational, not yet blocking
-	$options = array('http' => array(
-		'header'   => "Content-type: application/json\r\n",
-		'method'   => 'POST',
-		'content'  => '{"receiptRequested":"all",'.
-			'"tag":"signup'.$jcode.$jid.' jmp-register",'.
-			'"callbackUrl":"'.$fwdcalls_url.'",'.
-			'"from":"'.$support_number.'",'.
-			'"to":"'.$cheogram_did.'",'.
-			# TODO NOW: update this to add link/reply code 2 approve
-			'"text":"/msg '.$notify_pending_signup_jid.
-			' At '.gmdate("Y-m-d H:i:s").'Z user asking for JMP # '.
-			urlencode($_GET['number']).' from location '.
-			$_SERVER['REMOTE_ADDR'].' with JID '.$jid.
-			' requested registration - informational for now, but '.
-			'a reply will be needed in the future."}'
-	));
+	if (!$jid) {
+		$jidMaybeKey = 'reg-jid_maybe-'.$_GET['sid'];
+		$jid = $redis->get($jidMaybeKey);
 
-	$context = stream_context_create($options);
-	$result = file_get_contents("https://$tuser:$token".
-		'@api.catapult.inetwork.com/v1/users/'.
-		"$user/messages", false, $context);
-	if ($result === FALSE) {
+		if (!$jid) {
+			error_log('jError when verifying sid '.$_GET['sid']);
 ?>
 </head>
 <body>
 <p>
-There was an error sending your registration request.  Please <a href=
-"../registere/?number=<?php
-	echo urlencode($_GET['number']);
-?>&amp;sid=<?php
-	echo $clean_sid;
-?>&amp;jcode=<?php
-	echo $jcode;
-?>">click here</a> or press Reload to try again.
+Could not find JID to verify; perhaps it has already been verified.  Feel free
+to <a href="../">start again</a> if not.
+</p>
+<hr />
+<p>
+Copyright &copy; 2017, 2020 <a href="https://ossguy.com/">Denver Gingerich</a>
+and others.  jmp-register is licensed under AGPLv3+.  You can download the
+Complete Corresponding Source code <a
+href="https://gitlab.com/ossguy/jmp-register">here</a>.
+</p>
+</body>
+</html>
 <?php
-        } else {
+			exit;
+		}
+
+		$hitsKey = 'reg-jid_hits-'.$jid;
+		$hitCount = $redis->incr($hitsKey);
+
+		# if > 10 hits, do NOT allow verification to occur (rate limit)
+		if ($hitCount > 10) {
+			$ttl = $redis->ttl($hitsKey);
+			if ($ttl < 0) {
+				$redis->expire($hitsKey, 600);
+				# TODO: check return value
+			}
+
+			error_log('oError when trying to verify jid '.$jid);
 ?>
-<meta http-equiv="refresh" content="3;url=../register4/?number=<?php
+</head>
+<body>
+<p>
+Too many verification attempts.  Please refresh this page in about 10 minutes or
+<a href="../">start again</a>.
+</p>
+<hr />
+<p>
+Copyright &copy; 2017, 2020 <a href="https://ossguy.com/">Denver Gingerich</a>
+and others.  jmp-register is licensed under AGPLv3+.  You can download the
+Complete Corresponding Source code <a
+href="https://gitlab.com/ossguy/jmp-register">here</a>.
+</p>
+</body>
+</html>
+<?php
+			exit;
+		}
+
+		if (empty($_GET['jcode'])) {
+			error_log('vError for JID "'.$jid.'" - no verify code');
+?>
+</head>
+<body>
+<p>
+Verification code not entered.  Please press Back and enter a verification code
+or <a href="../">start again</a>.
+</p>
+<hr />
+<p>
+Copyright &copy; 2017, 2020 <a href="https://ossguy.com/">Denver Gingerich</a>
+and others.  jmp-register is licensed under AGPLv3+.  You can download the
+Complete Corresponding Source code <a
+href="https://gitlab.com/ossguy/jmp-register">here</a>.
+</p>
+</body>
+</html>
+<?php
+			exit;
+		}
+
+		$jcodeKey = 'reg-jcode-'.$jid;
+		$correct_jcode = $redis->get($jcodeKey);
+
+		$user_jcode = preg_replace('/[^0-9a-f]/', '', $_GET['jcode']);
+
+		if (strtolower($user_jcode) != $correct_jcode) {
+
+			if (strlen($_GET['number']) == 12 &&
+				$_GET['number'][0] == '+' &&
+				is_numeric(substr($_GET['number'], 1))) {
+
+				error_log('iError when trying to verify jid "'.
+					$jid.'" with jcode "'.$_GET['jcode'].
+					'" - should be "'.$correct_jcode.'"');
+?>
+</head>
+<body>
+<form action="../options1/">
+<p>
+<input type="hidden" name="number" value="<?php echo $_GET['number'] ?>" />
+<input type="hidden" name="sid" value="<?php echo $clean_sid ?>" />
+Invalid verification code (<?php echo $user_jcode ?>).  Please
+enter a new code to try again: <input type="text" name="jcode" />
+<input type="submit" value="Submit" />
+</p>
+</form>
+<?php
+			} else {
+				error_log('nError when trying to get number "'.
+					$_GET['number'].'" with JID "'.$jid.
+					'" and jcode "'.$_GET['jcode'].'"');
+?>
+</head>
+<body>
+<p>
+<?php
+				echo htmlentities($_GET['number']);
+?>
+ is not an E.164 NANP number.  Please <a href="../">start again</a>.
+</p>
+<?php
+			}
+?>
+<hr />
+<p>
+Copyright &copy; 2017, 2020 <a href="https://ossguy.com/">Denver Gingerich</a>
+and others.  jmp-register is licensed under AGPLv3+.  You can download the
+Complete Corresponding Source code <a
+href="https://gitlab.com/ossguy/jmp-register">here</a>.
+</p>
+</body>
+</html>
+<?php
+			exit;
+		}
+
+		# user jcode matches correct jcode, so we have verified the JID
+		$redis->setEx($jidDefinitelyKey, $key_ttl_seconds, $jid);
+		# TODO: check return value
+	}
+
+	# TODO NOW: give wait_for_approval1 as one option (not only) plus paymnt
+?>
+<meta http-equiv="refresh" content="3;url=../wait_for_approval1/?number=<?php
 	echo urlencode($_GET['number']);
 ?>&sid=<?php
 	echo $clean_sid;
-?>&jcode=<?php
-	echo $jcode;
 ?>" />
 </head>
 <body>
 
-<h2>Processing registration...</h2>
+<h2>Processing registration (part 1 of 2)...</h2>
 
 <p>
 If this page has been displayed for more than 5 seconds please <a href=
-"../register4/?number=<?php
+"../wait_for_approval1/?number=<?php
 	echo urlencode($_GET['number']);
 ?>&amp;sid=<?php
 	echo $clean_sid;
-?>&amp;jcode=<?php
-	echo $jcode;
 ?>">click here</a> to proceed.
 
 <?php
-	}
 }
 /*
 # TODO: update "== 12" for when we support non-NANPA numbers

register4/views/layout.erb
@@ -3,7 +3,7 @@
 	"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 
 <!--
-  Copyright (C) 2017  Denver Gingerich <denver@ossguy.com>
+  Copyright (C) 2017, 2020  Denver Gingerich <denver@ossguy.com>
 
   This file is part of jmp-register.
 
@@ -33,9 +33,9 @@
 </p>
 <hr />
 <p>
-Copyright &copy; 2017 <a href="https://ossguy.com/">Denver Gingerich</a> and
-others.  jmp-register is licensed under AGPLv3+.
-You can download the Complete Corresponding Source code <a
+Copyright &copy; 2017, 2020 <a href="https://ossguy.com/">Denver Gingerich</a>
+and others.  jmp-register is licensed under AGPLv3+.  You can download the
+Complete Corresponding Source code <a
 href="https://gitlab.com/ossguy/jmp-register">here</a>.
 </p>
 </body>

wait_for_approval1/index.php
@@ -1,0 +1,210 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+	"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<!--
+  Copyright (C) 2017, 2020  Denver Gingerich <denver@ossguy.com>
+
+  This file is part of jmp-register.
+
+  jmp-register is free software: you can redistribute it and/or modify it under
+  the terms of the GNU Affero General Public License as published by the Free
+  Software Foundation, either version 3 of the License, or (at your option) any
+  later version.
+
+  jmp-register is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+  details.
+
+  You should have received a copy of the GNU Affero General Public License along
+  with jmp-register.  If not, see <http://www.gnu.org/licenses/>.
+-->
+
+<html xmlns="http://www.w3.org/1999/xhtml"
+	xml:lang="en" >
+<head>
+<title>JMP</title>
+<?php
+
+if (empty($_GET['number'])) {
+	error_log('sError - no JMP number');
+?>
+</head>
+<body>
+<p>
+Number not entered.  Please <a href="../">start again</a>.
+<?php
+} elseif (empty($_GET['sid'])) {
+	error_log('sError - no session ID (sid) provided');
+?>
+</head>
+<body>
+<p>
+No session ID found.  Please <a href="../">start again</a>.
+<?php
+} else {
+	include '../../../../settings-jmp.php';
+
+	$redis = new Redis();
+	$redis->pconnect($redis_host, $redis_port);
+	if (!empty($redis_auth)) {
+		# TODO: check return value to confirm login succeeded
+		$redis->auth($redis_auth);
+	}
+
+	$jidDefinitelyKey = 'reg-jid_definitely-'.$_GET['sid'];
+	$jid = $redis->get($jidDefinitelyKey);
+
+	if (!$jid) {
+		# TODO: confirm below texts are correct
+		error_log('jError when verifying sid '.$_GET['sid']);
+?>
+</head>
+<body>
+<p>
+Could not find JID to verify; perhaps it has already been verified.  Feel free
+to <a href="../">start again</a> if not.
+</p>
+<hr />
+<p>
+Copyright &copy; 2017, 2020 <a href="https://ossguy.com/">Denver Gingerich</a>
+and others.  jmp-register is licensed under AGPLv3+.  You can download the
+Complete Corresponding Source code <a
+href="https://gitlab.com/ossguy/jmp-register">here</a>.
+</p>
+</body>
+</html>
+<?php
+		exit;
+	}
+
+	$clean_sid = preg_replace('/[^0-9a-f]/', '', $_GET['sid']);
+
+	if (strlen($_GET['number']) == 12 && $_GET['number'][0] == '+' &&
+		is_numeric(substr($_GET['number'], 1))) {
+
+		# send the registration request message: informational, no block
+		$options = array('http' => array(
+			'header'   => "Content-type: application/json\r\n",
+			'method'   => 'POST',
+			'content'  => '{"receiptRequested":"all",'.
+				'"tag":"signup'.$jid.' jmp-register",'.
+				'"callbackUrl":"'.$fwdcalls_url.'",'.
+				'"from":"'.$support_number.'",'.
+				'"to":"'.$cheogram_did.'",'.
+				# TODO NOW: add link/reply code for approving
+				'"text":"/msg '.$notify_pending_signup_jid.
+				' At '.gmdate("Y-m-d H:i:s").'Z wanting JMP # '.
+				htmlentities($_GET['number']).' from location '.
+				$_SERVER['REMOTE_ADDR'].' with JID '.$jid.
+				' - informational for now, but '.
+				'a reply will be needed in the future."}'
+		));
+
+		$context = stream_context_create($options);
+		$result = file_get_contents("https://$tuser:$token".
+			'@api.catapult.inetwork.com/v1/users/'.
+			"$user/messages", false, $context);
+		if ($result === FALSE) {
+?>
+</head>
+<body>
+<p>
+There was an error sending your registration request.  Please <a href=
+"../wait_for_approval1/?number=<?php
+	echo urlencode($_GET['number']);
+?>&amp;sid=<?php
+	echo $clean_sid;
+?>">click here</a> or press Reload to try again.
+<?php
+	        } else {
+			# TODO: XEP-0106 Sec 4.3 compliance; pre-escaped'll fail
+			$ej_search  = array('\\',  ' ',   '"',   '&',   "'",
+				'/',   ':',   '<',   '>',   '@');
+			$ej_replace = array('\5c', '\20', '\22', '\26', '\27',
+				'\2f', '\3a', '\3c', '\3e', '\40');
+			$cheo_jid = str_replace($ej_search, $ej_replace, $jid).
+				'@'.$cheogram_jid;
+
+			$now = time();
+
+			$ppaoKeyThisMo = 'payment-plan_as_of_'.
+				date('Ym', $now).
+				'-'.$cheo_jid;
+			$ppaoKeyNextMo = 'payment-plan_as_of_'.
+				date('Ym', strtotime('+1 month', $now)).
+				'-'.$cheo_jid;
+
+			# examples of valid values for payment-plan_as_of_.. key
+			#  cad_beta_unlimited-v20180430
+			#  xxx_stable_trial-v20200913
+			#   (XXX is '"transaction" involving no currency.' code)
+
+			# TODO: don't set until get approval (approval key TBD)
+
+			# TODO: don't hard-code expected payment plan name
+			if (!$redis->setnx($ppaoKeyThisMo,
+				'xxx_stable_trial-v20200913')) {
+
+				# note that register4 will deal with conflicts
+				$val = $redis->get($ppaoKeyThisMo);
+				error_log('zErrorPlan already set for this to '.
+					$val.' for JID '.$jid);
+			}
+
+ 			# TODO: don't hard-code expected payment plan name
+			if (!$redis->setnx($ppaoKeyNextMo,
+				'xxx_stable_trial-v20200913')) {
+
+				# note that register4 will deal with conflicts
+				$val = $redis->get($ppaoKeyNextMo);
+				error_log('zErrorPlan already set for next to '.
+					$val.' for JID '.$jid);
+			}
+?>
+<meta http-equiv="refresh" content="3;url=../register4/?number=<?php
+	echo urlencode($_GET['number']);
+?>&sid=<?php
+	echo $clean_sid;
+?>" />
+</head>
+<body>
+
+<h2>Processing registration (part 2 of 2)...</h2>
+
+<p>
+If this page has been displayed for more than 5 seconds please <a href=
+"../register4/?number=<?php
+	echo urlencode($_GET['number']);
+?>&amp;sid=<?php
+	echo $clean_sid;
+?>">click here</a> to proceed.
+
+<?php
+		}
+	} else {
+		error_log('nError when trying to reg number "'.$_GET['number'].
+			'" with JID "'.$jid.'" from session ID '.$clean_sid);
+?>
+</head>
+<body>
+<p>
+<?php
+				echo htmlentities($_GET['number']);
+?>
+ is not an E.164 NANP number.  Please <a href="../">start again</a>.
+<?php
+	}
+}
+?>
+</p>
+<hr />
+<p>
+Copyright &copy; 2017, 2020 <a href="https://ossguy.com/">Denver Gingerich</a>
+and others.  jmp-register is licensed under AGPLv3+.  You can download the
+Complete Corresponding Source code <a
+href="https://gitlab.com/ossguy/jmp-register">here</a>.
+</p>
+</body>
+</html>

commit 4f23284ce8ca854c890a81ee2ca61e59c9477147

fix empty sid for register5; regression in 96b6853
A user reported to me that they could not set a forwarding number
using register5 anymore.  It turned out this was because of a
regression in 96b6853, namely that @cleanSid had been deleted.  We
thought this was fine because it was no longer used anywhere in
reg_cheo.rb but failed to remember that it was still used in the
success.rb view.  So this restores @cleanSid, moving it to a more
appropriate location in the process (in hopes that it won't be
accidentally removed again, due to its proximity to other variables
that are of a similar nature).

register4/reg_cheo.rb
@@ -389,6 +389,7 @@ class SApp < Sinatra::Application
 		@jid = CGI.escapeHTML(jid)
 		@number = params['number']
 		@prefix = $catapult_domain_prefix
+		@cleanSid = params['sid'].gsub(/[^0-9a-f]/, "")
 
 		EM.stop
 