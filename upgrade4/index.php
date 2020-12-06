6956390ef8fca643c53a7d8a150f05b066cea2f4
Denver Gingerich <denver@ossguy.com>
2020-12-06 20:38:20 +0000

    update prices in ff55b47 (and ace10b6) - last >1mo

--- upgrade4/index.php
+++ upgrade4/index.php
@@ -43,7 +43,7 @@ ID not entered.  Please <a href="../upgrade1/">start again</a>.
 <p>
 Amount not entered.  Please <a href="../upgrade1/">start again</a>.
 <?php
-} elseif (intval($_GET['amount_sat']) < 200000) {
+} elseif (intval($_GET['amount_sat']) < 150000) {
 	error_log('mError - amount ('.$_GET['amount_sat'].') too low');
 ?>
 </head>
