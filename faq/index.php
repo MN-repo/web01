b3fc7025047f31457cc17cef14458fa04098e1ee
Stephen Paul Weber <singpolyma@singpolyma.net>
2021-04-05 08:32:47 -0500

    link to main project home page for all source code links

--- faq/index.php
+++ faq/index.php
@@ -67,7 +67,7 @@
 		<section id="about">
 			<h1>How do I learn more about JMP?</h1>
 
-			<p>All of the software that makes up JMP is <a href="https://en.wikipedia.org/wiki/Free_and_open-source_software">free and open source</a> software.  You can view, download, and modify the source code <a href="https://gitlab.com/users/ossguy/projects">here</a>.</p>
+			<p>All of the software that makes up JMP is <a href="https://en.wikipedia.org/wiki/Free_and_open-source_software">free and open source</a> software.  You can view, download, and modify the source code <a href="https://soprani.ca">here</a>.</p>
 			<p>For news about JMP, you can signup for our low-volume notification list <a href="https://soprani.ca/cgi-bin/mailman/listinfo/jmp-news">here</a>. You can also <a href="https://twitter.com/JMP_chat">follow us on Twitter</a>.</p>
 		</section>
 
