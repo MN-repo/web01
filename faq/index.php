1a95793ed41a5cab4a138e9b487b08ac54dd28f1
Stephen Paul Weber <singpolyma@singpolyma.net>
2021-04-26 20:52:23 -0500

    FAQ updates
    Remove some technical language and details, update some sections to more
    accurately describe the service today, point users to support more often than
    MUC PM, show a Jabber ID for support, use inner-links instead of duplicating
    information where reasonable.

--- faq/index.php
+++ faq/index.php
@@ -15,6 +15,14 @@
 			body > section h1:before {
 				content: "Q"counter(heading)". ";
 			}
+
+			iframe {
+				border: none;
+				width: 28rem;
+				height: 6rem;
+				background: #fff;
+				border-radius: 1em;
+			}
 		</style>
 	</head>
 
@@ -25,8 +33,7 @@
 		<section id="features">
 			<h1>What do I get when I signup for JMP?</h1>
 
-			<p>JMP gives you a Canadian or US phone number that is yours to keep (for <a href="https://soprani.ca/vonage/prices_by_country.html">46 other countries</a> you can use the <a href="https://wiki.soprani.ca/VonageSetup">the Vonage SGX</a>, also part of <a href="https://soprani.ca/">Soprani.ca</a>).  JMP allows you to send and receive text messages and picture messages using your <a href="#jabber_client">Jabber client</a>, with calls <a href="#voicemail"> delivered to voicemail</a> (including a text transcription) by default.</p>
-			<p>If you like, you can <a href="#calling">make and receive phone calls</a> using your <a href="https://arstechnica.com/business/2010/01/voip-in-depth-an-introduction-to-the-sip-protocol-part-1/">SIP</a> client (or receive with an existing Canadian or US phone number or directly in a supporting Jabber client).  You can <a href="#voicemail">configure a voicemail timeout</a> if you'd like JMP to automatically handle any calls that ring for too long.</p>
+			<p>JMP gives you a Canadian or US phone number that is yours to keep (for <a href="https://soprani.ca/vonage/prices_by_country.html">46 other countries</a> you can use the <a href="https://wiki.soprani.ca/VonageSetup">the Vonage SGX</a>, also part of <a href="https://soprani.ca/">Soprani.ca</a>).  JMP allows you to send and receive text messages and picture messages using your <a href="#jabber_client">Jabber client</a>.  You can also <a href="#calling">make and receive phone calls</a>, including receiving <a href="#voicemail">voicemails</a> delivered to you as audio recordings and text transcriptions.</p>
 		</section>
 
 		<section id="jabber">
@@ -41,8 +48,11 @@
 		<section id="sending">
 			<h1>How do I send text messages?</h1>
 
-			<p>Text messages are sent and received using special Jabber IDs (JIDs).  To send a text message, first add the JID representing the destination phone number to your roster.  For example, to send a text message to +1 416 993 8000 you would add "+14169938000@cheogram.com" to your roster.  You can then send the contact a message or picture and they will receive it as an SMS/MMS message.</p>
+			<p>Text messages are sent and received using special Jabber IDs.  To send a text message, first add the Jabber ID representing the destination phone number to your Jabber contact list.  For example, to send a text message to +1 416 993 8000 you would add "+14169938000@cheogram.com" to your contacts.  You can then send the contact a message or picture and they will receive it as an SMS/MMS message.</p>
 			<p>To send text messages to short codes, use the special suffix for short codes, ie. "33733;phone-context=ca-us.phone-context.soprani.ca@cheogram.com" represents the 33733 short code.  This will be simplified in the future, but is required for now in order to maintain proper uniqueness going forward.</p>
+
+			<p>From the same device as your <a href="#jabber_client">Jabber client</a> you can also add contacts using the form below:</p>
+			<iframe src="https://jabber-iq-gateway.api.cheogram.com/?to=cheogram.com"></iframe>
 		</section>
 
 
@@ -60,8 +70,8 @@
 			<h1>How do I get help with JMP?</h1>
 
 			<p>The best venue for help using or developing features for JMP is the Soprani.ca chatroom, which you can join <a href="xmpp:discuss@conference.soprani.ca?join">from your chat client</a> or <a href="https://anonymous.cheogram.com/discuss@conference.soprani.ca">on the web</a>.  It is an active and enthusiastic channel, and the fastest way to solve problems with the developers or other users.</p>
-			<p>If you'd like to send a private inquiry instead, or if you cannot join the chatroom, send a text message to the support team at <a href="sms:+14169938000">+1 416 993 8000</a> (Canada) or <a href="sms:+13127968000">+1 312 796 8000</a> (US) and we will get back to you within 8 business hours.  Both numbers can be texted from most other countries, though your carrier may charge a fee for international text messaging.</p>
-			<p>If you have found a bug in JMP, please file an issue at <a href="https://gitlab.com/ossguy/sgx-catapult/issues">https://gitlab.com/ossguy/sgx-catapult/issues</a>.</p>
+
+			<p>For private inquiries, contact support directly by Jabber at <a href="xmpp:+14169938000@cheogram.com">+14169938000@cheogram.com</a> or by SMS at <a href="sms:+14169938000">+1 416 993 8000</a> (Canada) or <a href="sms:+13127968000">+1 312 796 8000</a> (US) and we will get back to you within 8 business hours.  Both numbers can be texted from most other countries, though your carrier may charge a fee for international text messaging.</p>
 		</section>
 
 		<section id="about">
@@ -94,7 +104,7 @@
 
 			<p>Currently JMP supports calling to Canada and the US.</p>
 			<p>The easiest way is to make a call from your Jabber client, if you are using a supporting client such as Conversations, Siskin, Movim, or Gajim.  Simply add a contact just as you would for <a href="#sending">messaging</a> and then select the voice call option in your client.</a>
-			<p>Another way to make calls is to login to the SIP account that was provided during the signup process using a SIP client (we recommend <a href="https://f-droid.org/archive/com.csipsimple_2459.apk" >CSipSimple</a> for Android and <a href="https://itunes.apple.com/us/app/linphone/id360065638">Linphone</a> for iOS).  Then enter the 10-digit phone number that you'd like to call.</p>
+			<p>Another way to make calls is to login to the <a href="https://arstechnica.com/business/2010/01/voip-in-depth-an-introduction-to-the-sip-protocol-part-1/">SIP</a> account that was provided during the signup process using a SIP client (we recommend <a href="https://f-droid.org/archive/com.csipsimple_2459.apk" >CSipSimple</a> for Android and <a href="https://itunes.apple.com/us/app/linphone/id360065638">Linphone</a> for iOS).  Then enter the 10-digit phone number that you'd like to call.</p>
 			<p>If you don't have your SIP account information, you can reset your SIP account and receive the new password and other details by communicating with <a href="xmpp:cheogram.com">cheogram.com</a>, which should already be in your contacts.  In some clients you can find "Execute command" or "Actions" in the menus and select "Reset SIP Account" from there.  Otherwise send the message "help" and the bot will reply with a list of options.</p>
 		</section>
 
@@ -102,7 +112,7 @@
 			<h1>How does voicemail work?</h1>
 
 			<p>Calls will be delivered to voicemail if your SIP account (or forwarding number) is busy or not logged in.  Voicemails will be sent as messages to your XMPP client, both as an audio file as soon as the voicemail is left, and also as transcribed text once our transcription engine has finished converting the audio to text for you (normally this takes just a few seconds).</p>
-			<p>If SIP or forwarding is setup, you can configure calls to be sent to voicemail after a certain number of rings by using an XMPP ad-hoc command, which is done (ie. on a computer, using your JID in <a href="https://mov.im">Movim</a>) by clicking on cheogram.com in your contacts.  Under Actions, you can then choose to either "Configure Calls" (set the seconds until voicemail - a ring is about 5 seconds and "-1" means unlimited rings) or "Record Voicemail Greeting" (see below).</p>
+			<p>If SIP or forwarding is setup, you can configure calls to be sent to voicemail after a certain number of rings by using a command <a href="#bot">via bot or client UI</a>.</p>
 			<p>The default voicemail greeting is: "You have reached the voicemail of a user of <a href="https://jmp.chat/">JMP.chat</a>.  Please send a text message, or leave a message after the tone."  If your XMPP user <a href="https://xmpp.org/extensions/xep-0054.html">has a vCard</a> with FN or NICKNAME specified, then JMP will use that instead of "a user of <a href="https://jmp.chat/">JMP.chat</a>" in your voicemail greeting.  You can also set your own voicemail greeting using the "Record Voicemail Greeting" ad-hoc command (see above), which will call your SIP account (or forwarding number) to record the greeting.</p>
 		</section>
 
@@ -110,13 +120,13 @@
 		<section id="payment">
 			<h1>Do I have to use a credit card to signup for JMP?</h1>
 
-			<p>No.  Other payment methods are available.  For details, please contact our support team by sending a text message to <a href="sms:+14169938000">+1 416 993 8000</a> (Canada) or <a href="sms:+13127968000">+1 312 796 8000</a> (US) or send a private message to a moderator/owner in the JMP/Soprani.ca chatroom at <a href="xmpp:discuss@conference.soprani.ca?join">discuss@conference.soprani.ca</a> (just ask in the room if it's unclear who the moderators are).</p>
+			<p>No.  Other payment methods are available.  For details, please <a href="#support">contact our support team</a>.</p>
 		</section>
 
 		<section id="existing">
 			<h1>Can I port my existing number into JMP?</h1>
 
-			<p>Yes!  We support most regions in Canada and the US.  The process normally takes 1-2 weeks, depending on the number being ported in.  You can <a href="/sp1a/porting1/">submit your port request here</a>.  For questions, please reach our support team by sending a text message to <a href="sms:+14169938000">+1 416 993 8000</a> (Canada) or <a href="sms:+13127968000">+1 312 796 8000</a> (US) or send a private message to a moderator/owner in the JMP/Soprani.ca chatroom at <a href="xmpp:discuss@conference.soprani.ca?join">discuss@conference.soprani.ca</a> (just ask in the room if it's unclear who the moderators are).</p>
+			<p>Yes!  We support most regions in Canada and the US.  The process normally takes 1-2 weeks, depending on the number being ported in.  You can <a href="/porting1/">submit your port request here</a>.  For questions, please <a href="#support">contact our support team</a>.</p>
 		</section>
 
 		<section id="clients">
@@ -144,7 +154,6 @@
 			<h1>How many minutes and messages have I used this month?</h1>
 
 			<p>You can see how many minutes and outgoing messages you've used for each of the past several days or months, including a total, by sending 'u' to <a href="xmpp:cheogram.com">cheogram.com</a> which should already be in your contacts.</p>
-			<p>Note that usage is reported for <a href="https://en.wikipedia.org/wiki/Gregorian_calendar">Gregorian calendar</a> days using <a href="https://en.wikipedia.org/wiki/International_Atomic_Time">International Atomic Time (TAI)</a>.</p>
 			<p>Since JMP does not charge for nor count incoming message usage, only minutes and outgoing message usage are shown.</p>
 		</section>
 
