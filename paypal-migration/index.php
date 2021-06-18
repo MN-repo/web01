48758d12b4adfe6d5f586dded5fb4caf194eee35
Stephen Paul Weber <singpolyma@singpolyma.net>
2021-06-17 19:53:07 -0500

    Add paypal migration page

---
+++ paypal-migration/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>JMP: Migrate from PayPal</title>
+
+		<link rel="stylesheet" type="text/css" href="../style.css" />
+	</head>
+
+	<body>
+		<h1><img src="../static/jmp_beta.png" alt="JMP" /></h1>
+
+		<h1>How to switch your JMP account from PayPal to credit card</h1>
+
+		<p>JMP is encouraging (though at this time not requiring) people who currently pay for JMP using a PayPal subscription to switch to our new billing system.  The new billing system provides a number of benefits over PayPal, including the ability to pay for upcoming services like international calling, and extra minutes beyond those included in your plan.  All customers who pay by Bitcoin or other manual payment have been migrated to this system already.  We are now also providing incentives for those using a PayPal subscription to begin moving over as well.</p>
+
+		<p>In particular, customers who migrate from a PayPal subscription to our new billing system (using a credit card with auto top-up) will receive one free month of service.  Currently the new billing system uses a prepaid balance with auto top-ups.  To get your free month:</p>
+
+		<ol>
+			<li>Go to <a href="../upgrade1">JMP's account payment page</a></li>
+			<li>Enter your Jabber ID or JMP number</li>
+			<li>Click the "Pay with Credit Card" button</li>
+			<li>Enter your credit card information, and enter an amount of credit you would like added when your account balance gets low (minimum $15). <small>Note that larger amounts will receive bonus credit, up to 5% of the top-up - this roughly mirrors the effective bonus given to people who pay annually vs. monthly.</small></li>
+			<li>Click "Activate"</li>
+			<li>Go to <a href="https://www.paypal.com/myaccount/autopay/">https://www.paypal.com/myaccount/autopay/</a> and cancel your PayPal subscription to JMP</li>
+			<li><a href="/faq/#support">Contact support privately</a> to receive your free month of service — be sure to mention if you were a PayPal monthly or annual subscriber and include your PayPal email address.</li>
+		</ol>
+
+		<strong>Annual subscribers <em>must</em> contact support as part of this migration! Failure to do so could result in missing credit.</strong>
+
+		<?php require dirname(__FILE__).'/../nav.php'; ?>
+	</body>
+</html>
